c402e5d4e3b4a9c394befd7a1cff452cf0409876
Sergey Ladanov <ladanov@digitrans.ru>
2021-07-28 15:37:41 +0300

    Проверка ошибок

--- Easy_DataBase/Src/easy_db.cpp
+++ Easy_DataBase/Src/easy_db.cpp
@@ -52,9 +52,11 @@ int8_t EasyDataBase::Select(void)
 {
     uint8_t *readBuffer = new uint8_t[Row.Size()];
     uint32_t read_bytes = 0;
+    int8_t status = 0;
 
     SelectedMinId = 0xFFFFFFFF;
     SelectedMaxId = 0;
+    SelectedRowCount = 0;
 
     #if defined(_WIN32) || defined(_WIN64) || defined(__linux__)
     File = fopen(Name, "rb");
@@ -62,38 +64,64 @@ int8_t EasyDataBase::Select(void)
     if (File == nullptr)
     {
         printf("Failed to open file for selecting\r\n");
-        return -1;
+        status = -1;
     }
-
-    for (uint32_t i = 0; i < Capacity; i++)
-    {
-        read_bytes = fread(readBuffer, sizeof(uint8_t), Row.Size(), File);
-
-        if (read_bytes != Row.Size())
-        {
-            break;
-        }
-
-        Row.DeSerialize(readBuffer);
-
-        if (Row.RecordId > SelectedMaxId)
-        {
-            SelectedMaxId = Row.RecordId;
-        }
-
-        if (Row.RecordId < SelectedMinId)
-        {
-            SelectedMinId = Row.RecordId;
-        }
-    }
-
-    fclose(File);
     #endif
+
+    #if defined(__arm__)
+        
+    #endif
+
+    else
+    {
+
+        for (uint32_t i = 0; i < Capacity; i++)
+        {
+            #if defined(_WIN32) || defined(_WIN64) || defined(__linux__)
+            read_bytes = fread(readBuffer, sizeof(uint8_t), Row.Size(), File);
+            #endif
+
+            #if defined(__arm__)
+        
+            #endif
+
+            if (read_bytes != Row.Size())
+            {
+                break;
+            }
+
+
+            if (Row.DeSerialize(readBuffer) != Row.Size())
+            {
+                printf("Record checksum error\r\n");
+                continue;
+            }
+
+            SelectedRowCount++;
+
+            if (Row.RecordId > SelectedMaxId)
+            {
+                SelectedMaxId = Row.RecordId;
+            }
+
+            if (Row.RecordId < SelectedMinId)
+            {
+                SelectedMinId = Row.RecordId;
+            }
+        }
+
+        #if defined(_WIN32) || defined(_WIN64) || defined(__linux__)
+        fclose(File);
+        #endif
+
+
+    }
+  
 
 
     delete[] readBuffer;
 
-    return 0;
+    return status;
 }
 
 // Чтение выделенной строки
@@ -102,41 +130,60 @@ int8_t EasyDataBase::ReadSelectedRow(uint32_t index)
     uint8_t *readBuffer = new uint8_t[Row.Size()];
     uint32_t memIndex = ((SelectedMinId % Capacity) + index) % Capacity;
     uint32_t read_bytes = 0;
+    int8_t status = 0;
 
-    #if defined(_WIN32) || defined(_WIN64) || defined(__linux__)
-    File = fopen(Name, "rb");
-
-    if (File == nullptr)
+    for (uint32_t i = 0; i < Capacity; i++)
     {
-        printf("Failed to open file for writing row\r\n");
-        return -1;
+        #if defined(_WIN32) || defined(_WIN64) || defined(__linux__)
+        File = fopen(Name, "rb");
+
+        if (File == nullptr)
+        {
+            printf("Failed to open file for writing row\r\n"); 
+            status = -1;
+            break;
+        }
+
+        fseek(File, Row.Size() * memIndex, SEEK_SET);
+
+        read_bytes = fread(readBuffer, sizeof(uint8_t), Row.Size(), File);
+
+        fclose(File);
+
+        #endif
+
+        #if defined(__arm__)
+        
+        #endif
+
+        if (read_bytes != Row.Size())
+        {
+            printf("Read bytes no match with rowsize!\r\n");
+            printf("Read bytes: %d\r\n", read_bytes);
+            printf("Row size: %d\r\n", Row.Size());
+            status = -1;
+            break;
+        }
+
+        
+
+        if (Row.DeSerialize(readBuffer) != Row.Size())
+        {
+            printf("Record checksum error\r\n");
+            memIndex = (memIndex + 1) % Capacity;
+            continue;
+        }
+        else
+        {
+            printf("Row id: %d\r\n", Row.RecordId);
+            break;
+        }
+
     }
-
-    fseek(File, Row.Size() * memIndex, SEEK_SET);
-
-    read_bytes = fread(readBuffer, sizeof(uint8_t), Row.Size(), File);
-
-    fclose(File);
-
-    #endif
-
-    Row.DeSerialize(readBuffer);
-
-    printf("Row id: %d\r\n", Row.RecordId);
 
     delete[] readBuffer;
     
-
-    if (read_bytes != Row.Size())
-    {
-        printf("Read bytes no match with rowsize!\r\n");
-        printf("Read bytes: %d\r\n", read_bytes);
-        printf("Row size: %d\r\n", Row.Size());
-        return -1;
-    }
-
-
-    return 0;
+    return status;
 }
 
 // Запись строки в файл
